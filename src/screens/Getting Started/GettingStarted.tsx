a6b1ef8b0e003cdacdb7a12d4e23fe08c54785ea
Cazier92 <bryce.cazier@gmail.com>
2023-08-15 09:27:43 -0700

    update styling for getting started screen

--- src/screens/Getting Started/GettingStarted.tsx
+++ src/screens/Getting Started/GettingStarted.tsx
@@ -10,9 +10,9 @@ const GettingStarted = ({navigation}) => {
   return (
     <View className='flex-1 justify-center items-center'>
       <Text >iShareHealth</Text>
-      <Text >How can we help you today?</Text>
+      <Text className='mb-5'>How can we help you today?</Text>
 
-      <View className='flex-row flex-wrap m-0 justify-around items-center content-around gap-0'>
+      <View className='flex-row flex-wrap justify-around items-center content-around gap-0'>
         <GetStartedBtn text="Rapid Report: Share my results" />
         <GetStartedBtn onPress={() => navigation.navigate('Log In')} text="I need help taking my test" />
         <GetStartedBtn onPress={() => navigation.navigate('Log In')} text="What is my COVID-19 risk?" />
